b123ab6b047ad2f3534ee3c07bf44e3b7859163f
Piotr Pusz <petropusz@gmail.com>
2019-01-27 13:36:34 +0100

    changed jobs to python3

--- src/scripts/reproduce_snli.py
+++ src/scripts/reproduce_snli.py
@@ -5,19 +5,19 @@ from scheduling import launch
 
 jobs = [
     # SGD-CE
-    "python train_nli.py --opt sgd --eta 1 --loss ce --no-tqdm",
+    "python3 train_nli.py --opt sgd --eta 1 --loss ce --no-tqdm",
 
     # SGD-SVM
-    "python train_nli.py --opt sgd --eta 0.1 --loss svm --no-tqdm",
+    "python3 train_nli.py --opt sgd --eta 0.1 --loss svm --no-tqdm",
 
     # ADAM-SVM
-    "python train_nli.py --opt adam --eta 1e-4 --loss svm --no-tqdm",
+    "python3 train_nli.py --opt adam --eta 1e-4 --loss svm --no-tqdm",
 
     # ADAM-CE
-    "python train_nli.py --opt adam --eta 1e-4 --loss ce --no-tqdm",
+    "python3 train_nli.py --opt adam --eta 1e-4 --loss ce --no-tqdm",
 
     # # DFW-SVM
-    "python train_nli.py --opt dfw --eta 1 --loss svm --no-tqdm",
+    "python3 train_nli.py --opt dfw --eta 1 --loss svm --no-tqdm",
 ]
 
 
