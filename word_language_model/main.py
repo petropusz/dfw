3b0e3a866252c89fd1f5526e2d5ac6a6b75fec5a
R. Kaleta <rafkaleta@onet.pl>
2019-02-13 19:29:34 +0100

    Print gamma*lr for DFW

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -200,11 +200,12 @@ def train():
         total_loss += loss.item()
 
         if batch % args.log_interval == 0 and batch > 0:
+            learn_rate = optimizer.param_groups[0]["lr"] if args.optim == "SGD" else optimizer.param_groups[0]["lr"] * optimizer.gamma
             cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
-                      epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]["lr"],
+                      epoch, batch, len(train_data) // args.bptt, learn_rate,
                       elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
