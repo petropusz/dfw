6f8aa732c49e6babc044e250e3170247defcaff4
R. Kaleta <rafkaleta@onet.pl>
2019-02-13 20:29:45 +0100

    8 decimal points

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -203,7 +203,7 @@ def train():
             learn_rate = optimizer.param_groups[0]["lr"] if args.optim == "SGD" else optimizer.param_groups[0]["lr"] * optimizer.gamma
             cur_loss = total_loss / args.log_interval
             elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:08.8f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
                       epoch, batch, len(train_data) // args.bptt, learn_rate,
                       elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
