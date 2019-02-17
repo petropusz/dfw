d9bfa6e659f03514ecfca32bac97d22494d3707a
R. Kaleta <rafkaleta@onet.pl>
2019-02-17 18:37:10 +0100

    No ppl display

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -210,7 +210,7 @@ def train():
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:08.8f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
                       epoch, batch, len(train_data) // args.bptt, learn_rate,
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+                      elapsed * 1000 / args.log_interval, cur_loss, cur_loss))#, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
 
